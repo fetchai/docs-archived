699f6993beef64abfff6a77c79901ddd6dec28d5
Richard Bagnall @ Fetch.ai <56547751+bagnall-fetch@users.noreply.github.com>
2019-11-14 13:50:17 +0000

    Style fonts for menu

--- mkdocs-gitbook-0.0.1/mkdocs_gitbook/js/menu.js
+++ mkdocs-gitbook-0.0.1/mkdocs_gitbook/js/menu.js
@@ -1,10 +1,32 @@
-
-$("#summary").children().children().not(".head").toggle();
-$("#summary").on("click", ".header", function(event){
-    console.log('clicked');
-    $(this).children().not(".head").toggle();
-    //$(this).find('*').hide();
-    // $(this).next('li').next('li').toggle(200);
-    // $(this).next('li').next('li').next('li').toggle(200);
-    // $(this).next('li').next('li').next('li').next('ul').toggle(200);
+$('#summary .head').on('click', function() {
+    $(this)
+        .next()
+        .toggleClass('show');
 });
+
+$('#mkdocs-search-wrapper button').on('click', function() {
+    console.log('Close button clicked');
+    $('#mkdocs-search-wrapper').hide();
+});
+
+$('#mkdocs-search-query').on('keyup', function(ev) {
+    console.log('changey changey');
+    let t = setTimeout(function() {
+        let $searchResultsArticles = $('#mkdocs-search-results article');
+        console.log(2, $searchResultsArticles.length);
+        let $makedocsSearchWrapper = $('#mkdocs-search-wrapper');
+        let $searchInput = $('#mkdocs-search-query');
+        if ($searchResultsArticles.length) {
+            $makedocsSearchWrapper.css('display', 'flex');
+            $searchInput.addClass('pop-over');
+        } else {
+            $makedocsSearchWrapper.hide();
+            $searchInput.removeClass('pop-over');
+        }
+    }, 200); // Very hacky - I know!
+    let $searchResultsArticles = $('#mkdocs-search-results article');
+    console.log(1, $searchResultsArticles.length);
+    return true;
+});
+
+console.log('Load');
