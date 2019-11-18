afcfd8bfe417e76c64aecf788cfaf4d8ab1f464e
devjsc <joshscroft@gmail.com>
2019-11-18 10:21:56 +0000

    updated theme, and added sass deployment

--- setup.py
+++ setup.py
@@ -3,19 +3,12 @@ from setuptools import setup, find_packages
 VERSION = '0.0.1'
 
 setup(
-    name="mkdocs-gitbook",
-    version=VERSION,
-    url='https://gitlab.com/lramage/mkdocs-gitbook-theme',
-    license='Apache License, Version 2.0',
-    description='Default theme for GitBook for Mkdocs',
-    author='Lucas Ramage',
-    author_email='ramage.lucas@protonmail.com',
     packages=find_packages(),
     include_package_data=True,
     sass_manifests={
-        'package': {
-            'sass_path': '../../docs/sass',
-            'css_path': '../../docs/css',
+        '': {
+            'sass_path': 'docs/sass',
+            'css_path': 'docs/css',
             'strip_extension': True,
         },
     },
