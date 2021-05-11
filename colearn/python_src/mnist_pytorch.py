475575033aba1d27c5fc5fe2815acbf5601d4a30
Emma Smith <emma.smith@fetch.ai>
2021-05-11 09:00:17 +0000

    Deployed 1c326517 with MkDocs version: 1.1.2

--- colearn/python_src/mnist_pytorch.py
+++ colearn/python_src/mnist_pytorch.py
@@ -2,11 +2,11 @@
 #
 #   Copyright 2021 Fetch.AI Limited
 #
-#   Licensed under the Apache License, Version 2.0 (the "License");
-#   you may not use this file except in compliance with the License.
-#   You may obtain a copy of the License at
+#   Licensed under the Creative Commons Attribution-NonCommercial International
+#   License, Version 4.0 (the "License"); you may not use this file except in
+#   compliance with the License. You may obtain a copy of the License at
 #
-#       http://www.apache.org/licenses/LICENSE-2.0
+#       http://creativecommons.org/licenses/by-nc/4.0/legalcode
 #
 #   Unless required by applicable law or agreed to in writing, software
 #   distributed under the License is distributed on an "AS IS" BASIS,
